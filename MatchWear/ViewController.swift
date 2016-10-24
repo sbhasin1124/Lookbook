275f8ae81586749f24336fbf35457b9a230a5e04
Virtual Enterprise <VE@rm343wk30.local>
2016-10-24 09:39:20 -0400

    Initial Commit

---
+++ MatchWear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatchWear
+//
+//  Created by Virtual Enterprise on 10/24/16.
+//  Copyright © 2016 Virtual Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
